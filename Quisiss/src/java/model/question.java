e83034a56295219104b9c51c23af6738a795558c
Cheerrr_orz <tipsukon.cheer@mail.kmutt.ac.th>
2019-11-16 01:42:51 +0700

    [ADD] question model

---
+++ Quisiss/src/java/model/question.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Lenovo-Y50
+ */
+public class question {
+    private int quizid;
+    private int questionid;
+    private String question;
+    private String answer;
+
+    public question(int quizid, int questionid, String question, String answer) {
+        this.quizid = quizid;
+        this.questionid = questionid;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getQuizid() {
+        return quizid;
+    }
+
+    public void setQuizid(int quizid) {
+        this.quizid = quizid;
+    }
+
+    public int getQuestionid() {
+        return questionid;
+    }
+
+    public void setQuestionid(int questionid) {
+        this.questionid = questionid;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+    
+    
+}
